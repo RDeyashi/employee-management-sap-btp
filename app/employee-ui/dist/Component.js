bbc503cb796146e659293101c77a645009811af5
Rabi Deyashi <engg.r.deyashi@gmail.com>
2026-08-06 15:22:20 +0530

    feat: Official SAP Fiori Tools architecture with ui5-task-zipper and Managed HTML5 App Repository

--- app/employee-ui/dist/Component.js
+++
@@ -1,2 +0,0 @@
-sap.ui.define(["sap/fe/core/AppComponent"],function(e){"use strict";return e.extend("employee.management.ui.Component",{metadata:{manifest:"json"}})});
-//# sourceMappingURL=Component.js.map
